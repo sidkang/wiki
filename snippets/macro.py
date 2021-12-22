d45d6f93510a0be8e58e7a983d2f88f123658b84
kang <sid@mini.sid.pangmao.xyz>
2021-12-22 20:10:34 +0800

    modify plot script

--- snippets/macro.py
+++ snippets/macro.py
@@ -2,10 +2,12 @@ def define_env(env):
     """
     This is the hook for declaring variables, macros and filters (new form)
     """
-    import matplotlib.pyplot as plt
-    import base64
+    
+    from base64 import b64encode
     import io
     import warnings
+    import matplotlib.pyplot as plt
+    import matplotlib as mpl
 
     warnings.filterwarnings('ignore', '.*Matplotlib is currently using agg.*', )
 
@@ -20,6 +22,12 @@ def define_env(env):
     
     @env.macro
     def embed_plot(script_name, alt_text='', width=640, height=480):
+
+        mpl.rcParams['figure.dpi'] = 500
+        mpl.rcParams['savefig.dpi'] = 500
+        plt.style.use('seaborn-poster')
+
+        # sns.set_theme()
 
         plt.switch_backend('Agg')
         d = dict(locals(), **globals())
@@ -36,5 +44,5 @@ def define_env(env):
         buf = io.BytesIO()
         plt.tight_layout()
         plt.savefig(buf, format="png")
-        data = base64.b64encode(buf.getbuffer()).decode("ascii")
+        data = b64encode(buf.getbuffer()).decode("ascii")
         return f"<img alt='{alt_text}' width='{width}' height='{height}' src='data:image/png;base64,{data}'/>"
